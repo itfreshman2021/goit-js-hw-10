f151471bae76f8f7d0517a27cc85cbc98c58632a
Ruslan Filonchuk <itfreshman@ukr.net>
2021-11-26 20:20:03 +0200

    Додано функцію fetchCountries(name)
    Додано функцію fetchCountries(name)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import debounce  from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries.js';
 import './css/styles.css';
 
+
+
 const DEBOUNCE_DELAY = 300;
+
+let name = "tan"
+
+let x = fetchCountries(name);
+
+
